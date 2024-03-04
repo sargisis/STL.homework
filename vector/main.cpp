966e269cad40ccabc25ef7b51dad16a39e44f988
Sargis <157747247+sargisis@users.noreply.github.com>
2024-03-04 18:40:54 +0400

    main.cpp

---
+++ vector/main.cpp
@@ -0,0 +1,13 @@
+#include "Vector.hpp"
+
+int main(){
+Vector<int> intVector;
+ 
+    intVector.push_back(10);
+    intVector.push_back(20);
+    intVector.push_back(30);
+
+    intVector.print();
+    return 0;
+    
+}
